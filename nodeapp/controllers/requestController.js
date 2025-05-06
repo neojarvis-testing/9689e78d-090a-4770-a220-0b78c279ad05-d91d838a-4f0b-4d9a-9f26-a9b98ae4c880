58f5ebd794e40f0755db0f014f4f6fc24921bc51
kendyalasupriya <134077665+kendyalasupriya@users.noreply.github.com>
2025-05-07 00:31:50 +0530

    Update requestController.js
    nodeapp commit change

--- nodeapp/controllers/requestController.js
+++ nodeapp/controllers/requestController.js
@@ -44,8 +44,7 @@ exports.getRequestById = async (req, res) => {
 exports.getRequestsByUserId = async (req, res) => {
     try {
         const requests = await Request.find({ userId: req.params.userId })
-            .populate('feedId')
-            .populate('livestockId');
+           .populate([{path: 'feedId'}, {path: 'livestockId'}])
         res.status(200).json(requests);
     } catch (error) {
         res.status(500).json({ error: error.message });
