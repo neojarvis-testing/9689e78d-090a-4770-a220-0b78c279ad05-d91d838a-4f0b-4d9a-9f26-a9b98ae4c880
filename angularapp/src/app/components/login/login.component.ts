5cfcda4509a241973901cdd61abdac355f9d2d5e
kendyalasupriya <supriyakendyala@gmail.com>
2025-05-06 13:43:56 +0000

    login commit

---
+++ angularapp/src/app/components/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log('Form Data:', this.loginForm.value);
+    } else {
+      console.log('Invalid form submission');
+    }
+  }
+
+}
+
+
