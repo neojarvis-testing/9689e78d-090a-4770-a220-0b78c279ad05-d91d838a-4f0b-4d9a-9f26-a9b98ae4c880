15bf8f125605682e183ce8df7ef6fcc4bc1c26a9
saiaditya469 <srisaiadityaseela@gmail.com>
2025-05-09 19:15:54 +0000

    new data added in uploads

--- nodeapp/routers/liveStockRouter.js
+++ nodeapp/routers/liveStockRouter.js
@@ -7,7 +7,8 @@ router.get('/getLivestockById/:id',getLivestockById);
 router.get('/getLivestockByUserId/:id',getLivestockByUserId);
 // router.post('/addLivestock', upload.single('attachment'), addLivestock);
 router.post('/addLivestock', addLivestock);
-router.put('/updateLivestock/:id',updateLivestock);
+// router.put('/updateLivestock/:id',updateLivestock);
+router.put('/updateLivestock/:id',upload.single('attachment'),updateLivestock);
 router.delete('/deleteLivestock/:id',deleteLivestock)
 router.get('/getFileByLivestockId/:id/file',getFileByLivestockId);
 module.exports=router;
