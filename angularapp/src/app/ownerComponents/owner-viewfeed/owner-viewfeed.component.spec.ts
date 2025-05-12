15213ec0930a5f7764106a7fd5beb17851840e37
kendyalasupriya <supriyakendyala@gmail.com>
2025-05-12 14:16:23 +0000

    changes

--- angularapp/src/app/ownerComponents/owner-viewfeed/owner-viewfeed.component.spec.ts
+++ angularapp/src/app/ownerComponents/owner-viewfeed/owner-viewfeed.component.spec.ts
@@ -3,7 +3,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { OwnerViewfeedComponent } from './owner-viewfeed.component';
-import { of } from 'rxjs';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { By } from '@angular/platform-browser';
 
